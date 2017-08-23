7252e415f45c037044938472805ad116c7328c09
ErikYu <erikyuyz@gmail.com>
2017-08-23 23:35:32 +0800

    add chart api
    add chart api

--- app/api_1_0/controller.py
+++ app/api_1_0/controller.py
@@ -36,7 +36,8 @@ def get_record():
     month_chosen = request.args.get('month')
     month_range = get_month_start_end(month_chosen)
     # 查询一个时间段内所有记录，根据时间倒序排列
-    all_record = Diaryitem.query.filter(Diaryitem.time.between(month_range['start'], month_range['end'])).order_by(db.desc(Diaryitem.time)).all()
+    all_record = Diaryitem.query.filter(Diaryitem.time.between(month_range['start'], month_range['end'])).order_by(
+        db.desc(Diaryitem.time)).all()
     this_time = ''
     month_detail = []
     for record in all_record:
@@ -132,3 +133,68 @@ def verify_token():
     if User.verify_auth_token(token) is not None:
         return cross_origin({'confirmed': True})
     return cross_origin({'confirmed': False})
+
+
+# 8-18 增加类别
+@api.route('/create/category', methods=['POST'])
+def create_category():
+    # data = {
+    #     "name": u"吃饭",
+    #     "type": "out"
+    # }
+    new_category = json.loads(request.form.get('data'))
+    auth_token = request.form.get('token')
+    if User.verify_auth_token(auth_token) is not None:
+        categ = Project(name=new_category['name'],
+                        type=new_category['type'])
+        db.session.add(categ)
+        db.session.commit()
+        return cross_origin({'success': True})
+    return cross_origin({'success': False})
+
+
+# 提取所有关键字
+@api.route('/get/key/word', methods=['GET'])
+def get_key_word():
+    # 获取所有的日记项
+    all_item = Diaryitem.query.filter_by().all()
+    # 获取所有的类别
+    all_project = Project.query.filter_by().all()
+    project_id_name = {}
+    for project in all_project:
+        project_id_name[project.id] = project.name
+    print project_id_name
+    project_percent = {}
+    for item in all_item:
+        if project_percent.has_key(project_id_name[item.project_id]):
+            project_percent[project_id_name[item.project_id]] += item.price
+        else:
+            project_percent[project_id_name[item.project_id]] = item.price
+        # print item.project_id
+        # print item.price
+    print project_percent
+    return cross_origin(project_percent)
+
+
+# 获取指定月之间的月统计数据
+@api.route('/get/month/total', methods=['GET'])
+def get_month_total():
+    start_month = '2017-01'
+    end_month = '2017-08'
+    result_dict = {
+        '2017-01': 0,
+        '2017-02': 0,
+        '2017-03': 0,
+        '2017-04': 0,
+        '2017-05': 0,
+        '2017-06': 0,
+        '2017-07': 0,
+        '2017-08': 0
+    }
+    start_time = get_month_start_end(start_month)['start']
+    end_time = get_month_start_end(end_month)['end']
+    all_item = Diaryitem.query.filter(Diaryitem.time.between(start_time, end_time)).all()
+    for item in all_item:
+        key = str(item.time)[:7]
+        result_dict[key] += item.price
+    return cross_origin(result_dict)
